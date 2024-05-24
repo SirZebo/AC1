5492f5ab73d9cd931e94fa6da1b72ab570ffadc9
Farhan Azmi <68798786+farhanazmiCS@users.noreply.github.com>
2024-05-24 12:01:30 +0800

    Code cleanup fronend

--- frontend/Home.py
+++ frontend/Home.py
@@ -10,37 +10,6 @@ def main():
 
     st.sidebar.title('Steganographer')
 
-
-    st.markdown(
-    """
-    <style>
-    .sidebar .widget-title {
-        display: none;  /* Hide the title of the radio button */
-    }
-    .sidebar .stRadio > label {
-        display: block;
-        margin-bottom: 10px;
-        cursor: pointer;
-        border-radius: 5px;
-        background-color: #f0f0f0;
-        padding: 10px;
-    }
-    .sidebar .stRadio > label:hover {
-        background-color: #e0e0e0;
-    }
-    .sidebar .stRadio > label input[type="radio"] {
-        display: none;
-    }
-    .sidebar .stRadio > label input[type="radio"] + span {
-        position: relative;
-        top: 3px;
-        margin-right: 5px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
     page = st.sidebar.radio(
         label="Navigation", 
         options=["Steganography", "Analysis"],
